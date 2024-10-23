59d774ff56ac05cabd4cf7b83119d4f2033edfa0
Iago Oliveira Fonseca <iagooliveirafonseca83@gmail.com>
2024-10-23 11:10:03 -0300

    Finalizando o projeto

---
+++ script.js
@@ -0,0 +1,46 @@
+// Esse comentário, é uma tentativa de adicionar dinâmismo aos elementos Labels, por meio do input com o AddEventListener.
+// Consegui 50% que é adicionar, porém não obtive sucesso em remove-lo, ao sair do elemento input com o mouse.
+// Desta forma, terei que entregar o trabalho com essa parte faltando.. 
+
+// Coloquei alguns icones que eu me senti mais confortável, porém, é de efeito legal, por isso mantive a declaração no rodapé da página. 
+
+
+
+// const inputNameActive = document.querySelector('#name')
+// const inputEmailActive = document.querySelector('#email')
+// const labelNameActive = document.getElementsByTagName('label')[0]
+// const labelEmailActive = document.getElementsByTagName('label')[1]
+
+
+
+// inputNameActive.addEventListener('click',ChangeLabelColorName)
+// inputNameActive.addEventListener('onmouseout', RemoveLabelColorName)
+// inputEmailActive.addEventListener('click',ChangeLabelColorEmail)
+// inputEmailActive.addEventListener('onmouseout',RemoveLabelColorEmail)
+
+
+// function ChangeLabelColorName(){
+//   if(labelNameActive.classList != 'active'){
+//     labelNameActive.classList.add('active')
+//   }
+// }
+
+// function RemoveLabelColorName(){
+//     if(labelNameActive.classList == 'active'){
+//       labelNameActive.classList.remove('active')
+//     } 
+// }
+
+// function ChangeLabelColorEmail(){
+//   if(labelEmailActive.classList != 'active'){
+//     labelEmailActive.classList.add('active')
+//   }
+// }
+//   function RemoveLabelColorEmail(){
+//     if(labelEmailActive.classList == 'active'){
+//       labelEmailActive.classList.remove('active')
+//     } 
+//   }
+
+
+
